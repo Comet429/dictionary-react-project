aec95addd24ed977761efd4d096b16523eef0d1f
Jessie P. <ritime275@gmail.com>
2024-11-03 12:34:07 -0500

    Re-editied words meanings

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,18 +1,13 @@
 import React from "react";
 
 export default function Meanings(props) {
-  console.log(props.meanings);
   return (
     <div className="Meanings">
-      <h3>{props.word.meanings.partOfSpeech}</h3>
-      {props.meanings.definition.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p className="definition">{definition.definition}</p>
-            <p className="example">{props.meanings.example}</p>
-          </div>
-        );
-      })}
+      <h3>{props.meanings.partOfSpeech}</h3>
+      <div>
+        <p className="definition">{props.meanings.definition}</p>
+        <p className="example">{props.meanings.example}</p>
+      </div>
     </div>
   );
 }
