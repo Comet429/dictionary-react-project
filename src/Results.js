ebe641cfb0833d66513c997eeb531ce4ba2af048
Jessie P. <ritime275@gmail.com>
2024-11-09 04:52:15 -0500

    Added word phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Phonetic from "./Phonetic";
 import Meanings from "./Meanings";
 
 export default function Results(props) {
@@ -6,6 +7,10 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <div>
+          <Phonetic phonetic={props.results.phonetic} />
+        </div>
+
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
